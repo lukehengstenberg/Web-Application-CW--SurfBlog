6cc05ff955e5e9ed5256798f0b4719dd8bf92fcd
Luke Hengstenberg <lukehengstenberg@gmail.com>
2019-01-07 20:16:51 +0000

    Added Customer Error Reporting

---
+++ 878876/Controllers/ErrorsController.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+
+namespace _878876.Controllers
+{
+    [RequireHttps]
+    public class ErrorsController : Controller
+    {
+        [Route("Error/500")]
+        public IActionResult Error500()
+        {
+            var exceptionType = HttpContext.Features.Get<IExceptionHandlerPathFeature>();
+
+            if(exceptionType != null)
+            {
+                ViewBag.ErrorMessage = exceptionType.Error.Message;
+                ViewBag.RouteOfException = exceptionType.Path;
+            }
+
+            return View();
+        }
+
+        [Route("Error/{statusCode}")]
+        public IActionResult HandleErrorCode(int statusCode)
+        {
+            var statusCodeData = HttpContext.Features.Get<IStatusCodeReExecuteFeature>();
+
+            switch (statusCode)
+            {
+                case 404:
+                    ViewBag.ErrorMessage = "Error 404: Sorry the page you requested could not be found";
+                    ViewBag.RouteOfException = statusCodeData.OriginalPath;
+                    break;
+                case 500:
+                    ViewBag.ErrorMessage = "Error 500: Sorry something went wrong on the server";
+                    ViewBag.RouteOfException = statusCodeData.OriginalPath;
+                    break;
+            }
+
+            return View();
+        }
+    }
+}
